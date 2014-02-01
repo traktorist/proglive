010749981e6d0c9e548397d79359b1063bf3e5c3
Yaroslav Likhachev <traktor-lixach@rambler.ru>
2014-02-01 20:20:31 +0400

    подготовка исходников к заданию 5-го урока

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: traktor
- * Date: 29.01.14
- * Time: 20:16
- */
-
-// редирект на контроллер главной страницы
-header('Location: /controller/c_index.php');
-
-?>
+<?phpfunction __autoload($classname){	include_once("c/$classname.php");}$action = 'action_';$action .= (isset($_GET['act'])) ? $_GET['act'] : 'index';switch ($_GET['c']){	case 'articles':		$controller = new C_Page();		break;	default:		$controller = new C_Page();}$controller->Request($action);
