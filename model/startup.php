dd657c41a286a27d4155059f389507fa13cb384d
Yaroslav Likhachev <traktor-lixach@rambler.ru>
2014-02-13 22:14:20 +0400

    подготовка к заданию 7-го урока (исправлена кодировка и коннект БД)

--- model/startup.php
+++ model/startup.php
@@ -1,19 +1,18 @@
 <?php
 
-function startup()
-{
+function startup() {
 	// Настройки подключения к БД.
 	$hostname = 'localhost'; 
-	$username = 'root'; 
-	$password = '';
-	$dbName = 'test';
+	$username = 'proglive_php2';
+	$password = 'proglive_php2_pass';
+	$dbName = 'proglive_php2';
 	
 	// Языковая настройка.
-	setlocale(LC_ALL, 'ru_RU.CP1251');	
+	setlocale(LC_ALL, 'ru_RU.UTF8');
 	
 	// Подключение к БД.
-	mysql_connect($hostname, $username, $password) or die('No connect with data base'); 
-	mysql_query('SET NAMES cp1251');
+    $link = mysql_connect($hostname, $username, $password) or die('No connect with data base');
+    mysql_set_charset(ENCODING, $link); 													                                    // установка кодировки соединения с БД
 	mysql_select_db($dbName) or die('No data base');
 
 	// Открытие сессии.
